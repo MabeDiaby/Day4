207d23c83bfb54f71d38173bcf3108d29ce3b270
MabeDiaby <diabymabe@gmail.com>
2022-11-26 00:00:08 -0500

    refactor

--- practice/treasureMap.py
+++ practice/treasureMap.py
@@ -17,8 +17,7 @@ position = input("Where do you want to put the treasure? ")
 #Write your code below this row 👇
 horizontal = int(position[0])
 vertical = int(position[1])
-selected = map[vertical - 1]
-selected[horizontal - 1] = 'X'
+map[vertical - 1][horizontal - 1] = 'X'
 
 
 
