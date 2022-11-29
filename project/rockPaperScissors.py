a2d5404f2c9743ba71f054eebfe4f5c799686482
MabeDiaby <diabymabe@gmail.com>
2022-11-29 16:47:30 -0500

    refactor

--- project/rockPaperScissors.py
+++ project/rockPaperScissors.py
@@ -31,14 +31,15 @@ map = [rock, paper, scissors]
 choose = int(input("What do you choose? Type 0 for Rock, 1 for Paper, and 2 for Scissors.\n"))
 pickedComputer = random.randint(0, 2)
 # pickedComputer = map[randomComputer]
-chooseMe = map[(choose)]
-
-print(f"you picked : {chooseMe}")
-print(f"computer picked : {map[pickedComputer]}")
 
 if choose >=3 or choose < 0:
   print("invalid number, you lose. Try again!")
-elif choose == 0 and pickedComputer == 2:
+else:
+  chooseMe = map[(choose)]
+  print(f"you picked : {chooseMe}")
+  print(f"computer picked : {map[pickedComputer]}")
+
+if choose == 0 and pickedComputer == 2:
   print("You Win!")
 elif choose == 2 and pickedComputer == 0:
   print("Computer Wins!")
